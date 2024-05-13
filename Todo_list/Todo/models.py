af5a1fabf332e82aec03bfaffe12513e68b9764e
Alokbabuyadhuvanshi <124550479+Alokbabuyadhuvanshi@users.noreply.github.com>
2024-05-13 06:16:42 +0530

    adding files

---
+++ Todo_list/Todo/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=300)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
